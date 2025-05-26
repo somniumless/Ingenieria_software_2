8c4df5286c9d88415233b587f39d13ee92c2b630
somniumless <carlosperez12104524@gmail.com>
2025-05-25 19:03:52 -0500

    usuario añadido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\UsuarioController;
+
+Route::resource('usuarios', UsuarioController::class);
